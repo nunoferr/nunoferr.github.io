7e38511677b5d063974eba06489a559fe61829eb
Nuno Ferreira <nunoslmferreira@gmail.com>
2024-11-13 23:24:45 +0000

    fix: villagesTroopsCounter-test4 now working on worlds without scavenging.

--- TribalWars/Scripts/VillagesTroopsCounter/villagesTroopsCounter-test4.js
+++ TribalWars/Scripts/VillagesTroopsCounter/villagesTroopsCounter-test4.js
@@ -33,6 +33,7 @@
                 },
                 successMessage: 'Loaded successfully!',
                 loadingMessage: 'Loading...',
+                loadingWorldConfigMessage: 'Loading world config...',
                 credits: 'Village Troops Counter script v1.1.3 by NunoF- (.com.pt)'
             },
             pt_PT: {
@@ -47,6 +48,7 @@
                 },
                 successMessage: 'Carregado com sucesso!',
                 loadingMessage: 'A carregar...',
+                loadingWorldConfigMessage: 'A carregar configurações do mundo...',
                 credits: 'Contador de tropas em casa e em buscas v1.1.3 por NunoF- (.com.pt)'
             }
         };
@@ -57,14 +59,39 @@
         this.availableSupportUnits = Object.create(game_data.units);
         this.availableSupportUnits = Object.getPrototypeOf(this.availableSupportUnits);
         this.availableSupportUnits.splice(this.availableSupportUnits.indexOf('militia'), 1);
+        this.worldConfig = null;
+        this.isScavengingWorld = false
+        this.worldConfigFileName = `worldConfigFile${game_data.world}`;
     }
 
-    init() {
+    async init() {
         if (!game_data.features.Premium.active) {
             UI.ErrorMessage(this.UserTranslation.errorMessages.premiumRequired);
             return;
         }
+        await this.#initWorldConfig();
         this.#createUI();
+    }
+
+    async #initWorldConfig() {
+        var worldConfig = localStorage.getItem(this.worldConfigFileName);
+        if (worldConfig === null) {
+            UI.InfoMessage(this.UserTranslation.loadingWorldConfigMessage);
+            worldConfig = await this.#getWorldConfig();
+        } 
+        this.worldConfig = $.parseXML(worldConfig);
+        this.isScavengingWorld = this.worldConfig.getElementsByTagName('config')[0].getElementsByTagName('game')[0].getElementsByTagName('scavenging')[0].textContent.trim() === "1"; 
+    }
+
+    async #getWorldConfig() {
+        var Xml = this.#fetchHtmlPage('/interface.php?func=get_config');
+        localStorage.setItem(this.worldConfigFileName, (new XMLSerializer()).serializeToString(Xml));
+        await this.waitMilliseconds(Date.now(), 200);
+        return Xml;
+    }
+
+    async #waitMilliseconds(lastRunTime, milliseconds = 0) {
+        await new Promise(res => setTimeout(res, Math.max(lastRunTime + milliseconds - Date.now(), 0))); 
     }
 
     #generateUrl(screen, mode = null, extraParams = {}) {
@@ -103,7 +130,7 @@
 		return temp_data;
 	}
 
-    async #getTroopsObj() {
+    async #getTroopsScavengingWorldObj() {
         var troopsObj = {
             'villagesTroops': this.#initTroops(),
             'scavengingTroops': this.#initTroops()
@@ -131,12 +158,12 @@
                 });
             });
             currentPage++;
-        } while(true)
+        } while(true);
         
         return troopsObj;
 
         async function getScavengeMassScreenJson(currentObj, currentPage = 0, lastRunTime = 0) {
-            await new Promise(res => setTimeout(res, Math.max(lastRunTime + 200 - Date.now(), 0))); 
+            await currentObj.#waitMilliseconds(lastRunTime, 200);
             var html = currentObj.#fetchHtmlPage(currentObj.#generateUrl('place', 'scavenge_mass', {'page': currentPage}));
             var matches = html.match(/ScavengeMassScreen[\s\S]*?(,\n *\[.*?\}{0,3}\],\n)/);
             if (matches.length <= 1) {
@@ -149,6 +176,67 @@
             return JSON.parse(matches);
         }
     }
+
+    async #getTroopsNonScavengingWorldObj() {
+        var troopsObj = {
+            'villagesTroops': this.#initTroops(),
+            'scavengingTroops': this.#initTroops()
+        };
+		
+        var currentPage = 0;
+        var lastRunTime = Date.now();
+        this.#setMaxLinesPerPage(this, 'overview_villages', 'units', 1000);
+        this.#waitMilliseconds(lastRunTime, 200);
+        var lastVillageId = null;
+        do {
+            lastRunTime = Date.now();
+            var overviewTroopsPage = $.parseHTML(this.#fetchHtmlPage(this.#generateUrl('overview_villages', 'units', {'page': currentPage})));
+            var troopsTable = $(overviewTroopsPage).find('#units_table tbody');
+
+            var lastVillageIdTemp = $(troopsTable).find('span').eq(0).attr('data-id');
+            if (lastVillageId !== null && lastVillageId === lastVillageIdTemp) break; // an exact multiple of 1000 villages exist (i.e. 1000, 2000, 3000) and we are re-reading the last page.
+            lastVillageId = lastVillageIdTemp;
+
+            var currentObj = this;
+            $.each(troopsTable, function(id, tbodyObj) {
+                var villageTroops = $(tbodyObj).find('tr').eq(0);
+                var villageTroopsLine = $(villageTroops).find('td:gt(1)');
+                var c = 0;
+                $.each(currentObj.availableSupportUnits, function(key, value) {
+                    troopsObj.villagesTroops[value] += parseInt(villageTroopsLine.eq(c).text().trim());
+                    c++;
+                });
+            });
+            currentPage++;
+            this.#waitMilliseconds(lastRunTime);
+        } while(true);
+        
+        return troopsObj;
+    }
+
+    async #setMaxLinesPerPage(currentObj, screen, mode, value) {
+        await new Promise(res => setTimeout(res, Math.max(200, 0))); // Ensure that the 5 requests per second rule isnt broken
+
+        var form = document.createElement("form");
+        form.method = "POST";
+        form.action = "#"
+
+        $.each({page_size: value, h: game_data.csrf}, function (key, value) {
+            var input = document.createElement('input');
+            input.name = key;
+            input.value = value;
+            form.appendChild(input);
+        });
+
+        var dataString = $(form).serialize();
+        $.ajax({
+            type: 'POST',
+            url: currentObj.#generateUrl(screen, mode, { 'action': 'change_page_size', 'type': 'all' }),
+            data: dataString,
+            async: false
+        });
+    }
+
 
 	#getGroupsObj() {
 		var html = $.parseHTML(this.#fetchHtmlPage(this.#generateUrl('overview_villages', 'groups', {'type': 'static'})));
@@ -171,7 +259,7 @@
 
     async #createUI() {
         UI.InfoMessage(this.UserTranslation.loadingMessage);
-        var troopsObj = await this.#getTroopsObj();
+        var troopsObj = this.isScavengingWorld ? await this.#getTroopsScavengingWorldObj() : await this.#getTroopsNonScavengingWorldObj();
 		var html = `
 <div>
 <br>
@@ -184,8 +272,8 @@
                 ${getTroopsHeader(this.availableSupportUnits)}
             </thead>
             <tbody>
-                ${getTroopsLine(this.UserTranslation.home, troopsObj.villagesTroops)}
-                ${getTroopsLine(this.UserTranslation.scavenging, troopsObj.scavengingTroops)}
+                ${(this.isScavengingWorld) ? getTroopsLine(this.UserTranslation.home, troopsObj.villagesTroops) : ''}
+                ${(this.isScavengingWorld) ? getTroopsLine(this.UserTranslation.scavenging, troopsObj.scavengingTroops) : ''}
                 ${getTroopsLine(this.UserTranslation.total, troopsObj, 1)} 
             </tbody>
         </table>
@@ -349,7 +437,7 @@
 				return troops;
 			});
 
-			var html = `<tr><td class="center">${translation}</td>`;
+			var html = `<tr><td class="center" style="text-wrap: nowrap;">${translation}</td>`;
 			$.each(troops(), function(key, value) {
 				html += `<td class="center" data-unit="${key}">${value}</td>`;
 			});
@@ -358,7 +446,7 @@
 		}
 
 		function getTroopsHeader(availableSupportUnits) {
-			var html = `<tr><th class="center"></th>`;
+			var html = `<tr><th class="center" style="width: 0px;"></th>`;
 			$.each(availableSupportUnits, function(key, value) {
 				html += `<th style="text-align:center" width="35"><a href="#" class="unit_link" data-unit="${value}"><img src="https://dspt.innogamescdn.com/asset/2a2f957f/graphic/unit/unit_${value}.png" class=""></a></th>`;
 			});
