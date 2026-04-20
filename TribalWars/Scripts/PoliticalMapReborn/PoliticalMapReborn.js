eaaa5beba0ff43ebb5500ee04d233d3d7b5945f6
Nuno Ferreira <nunoslmferreira@gmail.com>
2026-04-20 01:38:41 +0100

    version number update

--- TribalWars/Scripts/PoliticalMapReborn/PoliticalMapReborn.js
+++ TribalWars/Scripts/PoliticalMapReborn/PoliticalMapReborn.js
@@ -1,6 +1,6 @@
 /*
  * Script Name: Political Map Reborn
- * Version: v0.1.8
+ * Version: v0.1.9
  * Last Updated:
  * Author: NunoF-
  * Author URL: https://nunoferr.github.io/
@@ -856,7 +856,7 @@ if (typeof politicalMapReborn !== 'undefined') {
           settingsLink: 'Political Map Reborn Settings',
           reloadButton: 'Reload Political Map Reborn',
           runPoliticalMapReborn: 'Run Political Map Reborn',
-          credits: 'Political Map Reborn script v0.1.8 by NunoF-',
+          credits: 'Political Map Reborn script v0.1.9 by NunoF-',
         },
         pt_PT: {
           informationMessages: {
@@ -932,7 +932,7 @@ if (typeof politicalMapReborn !== 'undefined') {
           settingsLink: 'Configurações do Political Map Reborn',
           reloadButton: 'Recarregar Political Map Reborn',
           runPoliticalMapReborn: 'Executar Political Map Reborn',
-          credits: 'Script Political Map Reborn v0.1.8 por NunoF-',
+          credits: 'Script Political Map Reborn v0.1.9 por NunoF-',
         },
       };
     }
